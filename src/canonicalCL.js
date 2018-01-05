9c0db42a745dea380ae29f69ab1f58445e4bb02a
ameeple <joyce.t.wu@gmail.com>
2018-01-04 18:22:46 -0800

    fix denver bug

--- src/canonicalCL.js
+++ src/canonicalCL.js
@@ -1,21 +1,25 @@
 export default function canonicalCL(urlIn) {
   const match = /^((http[s]?):\/\/)([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.exec(urlIn);
 
+  console.log('match: ' + match);
   if (!match || !/^[a-z]*\.craigslist\.org$/.exec(match[3])) {
     return null;
   }
 
-  const pathMatch = /^(\/[a-z]{3}\/[a-z]{3}\/)(d.*\/)?$/.exec(match[4]);
+  const pathMatch = /^(\/([a-z]{3}\/)*)(d.*\/)?$/.exec(match[4]);
 
+  console.log('pathmatch: ' + pathMatch);
   if (!pathMatch) {
     return null;
   }
 
   const fileMatch = /^(.*\/)?([0-9]*\.html)$/.exec(match[6]);
 
+  console.log('fileMatch: ' + fileMatch);
   if (!fileMatch) {
     return null;
   }
+  console.log('match(3):' + match[3] + ' pathMatch: ' + pathMatch[1] + ' fileMatch:' + fileMatch[2]);
 
   return 'https://' + match[3] + pathMatch[1] + fileMatch[2];
 }
--- test/library.spec.js
+++ test/library.spec.js
@@ -10,9 +10,9 @@ const expect = chai.expect;
 describe('canonicalURL', () => {
   it('should return a valid shortCL URL when given a valid long CL URL', () => {
     const canonicalURL =
-      canonicalCL('https://seattle.craigslist.org/est/cto/d/see-and-leave-reviews-on-cl/6408661519.html');
-
-    expect(canonicalURL).to.be.equal('https://seattle.craigslist.org/est/cto/6408661519.html');
+      canonicalCL('https://denver.craigslist.org/apa/d/500-off-february-rent-private/6448318921.html');
+      
+    expect(canonicalURL).to.be.equal('https://denver.craigslist.org/apa/6448318921.html');
   });
   it('should return a valid shortCL URL when given a valid short CL URL', () => {
     const canonicalURL = canonicalCL('https://seattle.craigslist.org/est/cto/6408661519.html');
